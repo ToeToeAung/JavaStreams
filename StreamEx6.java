9bd74168dbbf13212874aa07f3282eae74739297
Toe Toe Aung <toetoeag@gmail.com>
2024-07-12 21:32:02 -0500

    Exercise 7

---
+++ StreamEx6.java
@@ -0,0 +1,11 @@
+package Streams;
+
+import java.util.stream.Stream;
+
+public class StreamEx6 {
+    public static void main(String[] args) {
+        Integer[] arrOfInteger = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        Stream<Integer> strOfInt =Stream.of(arrOfInteger);
+        
+    }
+}
